4c803222be686a84111726d24afe3744be5321cb
Raiii <albengacayan486@gmail.com>
2025-12-18 17:58:20 +0800

    migrate

--- app/src/main/java/com/example/bloodbank/Email/Util.java
+++
@@ -1,7 +0,0 @@
-package com.example.bloodbank.Email;
-
-public class Util {
-
-    public static final String EMAIL = "ishiney741@gmail.com";
-    public static final String PASSWORD = "ptzzrryjpgblwjej";
-}
